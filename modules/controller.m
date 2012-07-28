cd4cf5a886d9e2ad9028baef7da08a265a94ec40
Karl Mikaelsson <derfian@lysator.liu.se>
2012-07-28 11:51:26 +0200

    Indent

--- modules/controller.m
+++ modules/controller.m
@@ -44,22 +44,22 @@
 	/endif
 
 /def -p2147483647 -mregexp -t'\[[ ]+[0-9]+[ ]+([A-z]+)[ ]+\]' GettingCharInfo03 = \
-  /let charclassI=%{P1}%;\
-  /if (regmatch({char}, {*})) \
-    /GettingCharClass %{charclassI}%;\
-    /set charclass=%{charclassI}%;\
-    /lood modules/loads.m%;\
-    /autodefineautoass%;\
-    /set welcomemsg2=Since you are SINGLE classed, and not 50/50 this script won't run perfectly until you are lvl 50/50! (btw, when you multiclass, you need to tell the script you're a new class or pure classed. Do this with the command %{htxt2}/config c te/ro%{ntxt2}.)%;\
-    /set welcomemsg3=Print %{htxt2}/uhelp %{ntxt}for some help, and %{htxt2}/menu %{ntxt}to change settings. Since this script needs a %{htxt2}special prompt%{ntxt} on burning you can type '%{htxt2}newprompt%{ntxt}' to get it!%;\
-    /set newprt display \\\%\r\\\%\h\(\\\%H\)H \\\%\p(\\\%\P)M \\\%\m(\\\%\M)V > \\\%\L%;\
-    /not /alias newprompt /not \\\%newprt%;\
-    /purge GettingCharInfo02%;\
-    /purge GettingCharInfo03%;\
-    /purge GettingCharClass%;\
-    /purge UhmGetCharInfo%;\
-    /repeat -0:00:01 1 /welcomemsg%;\
-  /endif
+        /let charclassI=%{P1}%;\
+        /if (regmatch({char}, {*})) \
+                /GettingCharClass %{charclassI}%;\
+                /set charclass=%{charclassI}%;\
+                /lood modules/loads.m%;\
+                /autodefineautoass%;\
+                /set welcomemsg2=Since you are SINGLE classed, and not 50/50 this script won't run perfectly until you are lvl 50/50! (btw, when you multiclass, you need to tell the script you're a new class or pure classed. Do this with the command %{htxt2}/config c te/ro%{ntxt2}.)%;\
+                /set welcomemsg3=Print %{htxt2}/uhelp %{ntxt}for some help, and %{htxt2}/menu %{ntxt}to change settings. Since this script needs a %{htxt2}special prompt%{ntxt} on burning you can type '%{htxt2}newprompt%{ntxt}' to get it!%;\
+                /set newprt display \\\%\r\\\%\h\(\\\%H\)H \\\%\p(\\\%\P)M \\\%\m(\\\%\M)V > \\\%\L%;\
+                /not /alias newprompt /not \\\%newprt%;\
+                /purge GettingCharInfo02%;\
+                /purge GettingCharInfo03%;\
+                /purge GettingCharClass%;\
+                /purge UhmGetCharInfo%;\
+                /repeat -0:00:01 1 /welcomemsg%;\
+        /endif
 
 /set animist=0
 /set fighter=0
@@ -71,31 +71,31 @@
 /set templar=0
 
 /def GettingCharClass = \
-  /if ({1} =/ 'an') \
-    /set animist=$[{animist}+1]%;\
-  /elseif ({1} =/ 'fi') \
-    /set fighter=$[{fighter}+1]%;\
-  /elseif ({1} =/ 'ma') \
-    /set magician=$[{magician}+1]%;\
-  /elseif ({1} =/ 'nb') \
-    /set nightblade=$[{nightblade}+1]%;\
-  /elseif ({1} =/ 'te') \
-    /set templar=$[{templar}+1]%;\
-  /elseif ({1} =/ 'pr') \
-    /set priest=$[{priest}+1]%;\
-  /elseif ({1} =/ 'ro') \
-    /set rogue=$[{rogue}+1]%;\
-  /elseif ({1} =/ 'wl') \
-    /set warlock=$[{warlock}+1]%;\
-  /else \
-    /ecko Error: Illegal Class: %htxt2%{1}%ntxt. (Bug in the script >;>).%;\
-  /endif
+        /if ({1} =/ 'an') \
+                /set animist=$[{animist}+1]%;\
+        /elseif ({1} =/ 'fi') \
+                /set fighter=$[{fighter}+1]%;\
+        /elseif ({1} =/ 'ma') \
+                /set magician=$[{magician}+1]%;\
+        /elseif ({1} =/ 'nb') \
+                /set nightblade=$[{nightblade}+1]%;\
+        /elseif ({1} =/ 'te') \
+                /set templar=$[{templar}+1]%;\
+        /elseif ({1} =/ 'pr') \
+                /set priest=$[{priest}+1]%;\
+        /elseif ({1} =/ 'ro') \
+                /set rogue=$[{rogue}+1]%;\
+        /elseif ({1} =/ 'wl') \
+                /set warlock=$[{warlock}+1]%;\
+        /else \
+                /ecko Error: Illegal Class: %htxt2%{1}%ntxt. (Bug in the script >;>).%;\
+        /endif
 
 ;;;;;;; 
 ;Select a character:
     
 /edit -p99999 ~login_hook_lp
-    
+
 /def -ip2147483647 -T'diku.uzi' -hLOGIN uzi_login = \
         /if ($[world_info("character")] =/ 'old') \
                 /send old%;\
@@ -108,28 +108,28 @@
         /endif
 
 /def -mglob -h"PROMPT *Press return to continue*" definesendsaveonprmpt = \
-  /send  %;/send 1%;/set gager=0%;/repeat -0:00:20 1 /gags
+        /send  %;/send 1%;/set gager=0%;/repeat -0:00:20 1 /gags
 
 
 
 /def -mglob -p99999 -t'Reconnecting.' UhmGetCharInfo = \
-  /repeat -0:00:01 1 /ecko   ...type %{htxt}SAVE %{ntxt}if you want Uzi to Kick in!%;\
-  /repeat -0:00:01 1 /ecko Uzi will not try to autoload anything at the moment... (Cause it looks like you just reconnected... can cause deaths.)%;\
+        /repeat -0:00:01 1 /ecko   ...type %{htxt}SAVE %{ntxt}if you want Uzi to Kick in!%;\
+        /repeat -0:00:01 1 /ecko Uzi will not try to autoload anything at the moment... (Cause it looks like you just reconnected... can cause deaths.)%;\
 
 /def -mglob -p9 -t'Welcome to the land of Burning! May your visit here be... Interesting.' welcometoburning = \
-  /send save
+        /send save
 
 /def welcomemsg = \
-  /sstate%;\
-  /d normal%;\
-  /let _wrapsize=%{wrapsize}%;\
-  /let _wrapspace=%{wrapspace}%;\
-  /set wrapsize=55%;\
-  /set wrapspace=5%;\
-  /eval /echo -a -p %{ntxt}     %{welcomemsg}   %{welcomemsg2}   %{welcomemsg3}%;\
-  /set wrapsize=%_wrapsize%;\
-  /set wrapspace=%_wrapspace%;\
-  /purge welcomemsg
+        /sstate%;\
+        /d normal%;\
+        /let _wrapsize=%{wrapsize}%;\
+        /let _wrapspace=%{wrapspace}%;\
+        /set wrapsize=55%;\
+        /set wrapspace=5%;\
+        /eval /echo -a -p %{ntxt}     %{welcomemsg}   %{welcomemsg2}   %{welcomemsg3}%;\
+        /set wrapsize=%_wrapsize%;\
+        /set wrapspace=%_wrapspace%;\
+        /purge welcomemsg
 
 ;;;;;;;
 
